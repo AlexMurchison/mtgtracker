7d7c041d0f0e4695b8992e3d250fed416d8f6edb
unknown <murchaldo@gmail.com>
2018-08-12 19:13:28 -0600

    Tracker Base

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 
+4 Distinct zones: Hand, Library,(bottom of library?) Graveyard, Exile.
+User can pull unique cards from database, add them to a "deck" array. 
+Add property to objects to denote number of cards in array.
+Add functions for buttons to decrease number in zone by 1, while increasing number in a separate zone by 1.
+All unique cards exist in each zone, hidden when number in zone is 0.
+Cards show up in a list in each zone, sorted by CMC.
+*/
